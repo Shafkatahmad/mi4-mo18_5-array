691fd118b6247d884c7cf03debf70296225e3ea7
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-13 21:02:17 +0600

    array

--- array.js
+++ array.js
@@ -1,1 +1,4 @@
-const numbers = []
+const numbers = [12, 22, 54, 65];
+const friends = ['Rohim', 'Korim', 'Rahmat'];
+const passed = [true, false, false, true];
+const mixedArray = [12, 'phnom penh', true, 84.4];
