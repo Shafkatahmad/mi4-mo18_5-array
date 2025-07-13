f5056aee16f65df40807f6310e093c7d80ac2c67
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-13 22:14:23 +0600

    push pop shift unshift

---
+++ pop.js
@@ -0,0 +1,6 @@
+const friends = ['John', 'Mike', 'Steven', 'Liam', 'Elijah'];
+// friends.push('Phillip');
+console.log(friends);
+const ditched = friends.pop();
+console.log(friends);
+console.log(ditched);
---
+++ shift-unshift.js
@@ -0,0 +1,6 @@
+const friends = ['John', 'Mike', 'Steven', 'Liam', 'Elijah'];
+
+friends.shift();
+console.log(friends);
+friends.unshift('John');
+console.log(friends);
